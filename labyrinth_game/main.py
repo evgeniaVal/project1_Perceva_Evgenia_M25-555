b982023151dab60db700519cbab7514c126bddb7
evgeniaVal <perceva221@gmail.com>
2026-01-08 17:02:28 +0300

    form arch and data

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+from .constants import ROOMS
+
+
 def main() -> None:
     print("Первая попытка запустить проект!")
+    game_state = {
+        'player_inventory': [], # Инвентарь игрока
+        'current_room': 'entrance', # Текущая комната
+        'game_over': False, # Значения окончания игры
+        'steps_taken': 0 # Количество шагов
+    }
 
 if __name__ == "__main__":
     main()
