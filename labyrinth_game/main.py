7097147c187be2111bf427a99d514819c221bd38
evgeniaVal <perceva221@gmail.com>
2026-01-08 17:28:24 +0300

    add get_input()

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -6,7 +6,7 @@ from .utils import describe_current_room
 
 
 def main() -> None:
-    print("Первая попытка запустить проект!")
+    print('Добро пожаловать в Лабиринт сокровищ!')
     game_state = {
         'player_inventory': [], # Инвентарь игрока
         'current_room': 'entrance', # Текущая комната
@@ -14,6 +14,8 @@ def main() -> None:
         'steps_taken': 0 # Количество шагов
     }
     describe_current_room(game_state)
+    while not game_state['game_over']:
+        pass
     show_inventory(game_state)
 
 if __name__ == "__main__":
--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -6,3 +6,11 @@ def show_inventory(game_state):
         print("В вашем инвентаре:")
         for item in inventory:
             print(f"- {item}")
+
+def get_input(prompt="> "):
+    try:
+        input_str = input(prompt)
+        return input_str.strip()
+    except (KeyboardInterrupt, EOFError):
+        print("\nВыход из игры.")
+        exit()
